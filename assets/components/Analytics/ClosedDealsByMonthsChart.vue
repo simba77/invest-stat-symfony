598cef31ebc587aac0036f9999bc34cb793e516c
simba77 <simba7@inbox.ru>
2024-12-15 12:14:24 +0300

    Updated charts

--- assets/components/Analytics/ClosedDealsByMonthsChart.vue
+++ assets/components/Analytics/ClosedDealsByMonthsChart.vue
@@ -1,27 +1,55 @@
 <script setup lang="ts">
-import { Chart } from 'chart.js/auto'
 import { onMounted, ref } from "vue";
+import * as echarts from 'echarts/core';
+import { GridComponent, TooltipComponent } from 'echarts/components';
+import { BarChart } from 'echarts/charts';
+import { CanvasRenderer } from 'echarts/renderers';
 
-const props = defineProps<{profitByMonths: number[]}>()
+const props = defineProps<{profitByMonths: object}>()
 const chartRef = ref()
 
+echarts.use([TooltipComponent, GridComponent, BarChart, CanvasRenderer])
+
 onMounted(() => {
-    new Chart(
-      chartRef.value,
-      {
-        type: 'bar',
-        data: {
-          labels: Object.keys(props.profitByMonths),
-          datasets: [
-            {
-              label: 'Profit by Months',
-              data: props.profitByMonths,
-              backgroundColor: '#ff4d73'
-            }
-          ]
-        }
+  const myChart = echarts.init(chartRef.value)
+  const option = {
+    color: [
+      '#ff4d73'
+    ],
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
       }
-    );
+    },
+    xAxis: {
+      type: 'category',
+      data: Object.keys(props.profitByMonths),
+      axisTick: {
+        alignWithLabel: true
+      },
+      axisLabel: {
+        rotate: 60
+      }
+    },
+    yAxis: {
+      type: 'value',
+    },
+    grid: {
+      left: '2%',
+      right: '2%',
+      bottom: '2%',
+      containLabel: true
+    },
+    series: [
+      {
+        data: Object.values(props.profitByMonths),
+        type: 'bar'
+      }
+    ]
+  }
+
+  myChart.setOption(option)
 })
 
 </script>
@@ -29,7 +57,7 @@ onMounted(() => {
 <template>
   <div>
     <div class="mb-7">
-      <canvas ref="chartRef" />
+      <div ref="chartRef" style="width: 100%; height: 700px;" />
     </div>
   </div>
 </template>
