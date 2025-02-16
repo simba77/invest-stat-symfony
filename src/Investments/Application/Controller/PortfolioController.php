700ede1b82358acdba76a92562fceeef29937dbc
simba77 <simba7@inbox.ru>
2025-02-16 12:45:35 +0300

    Fix route definition

--- src/Investments/Application/Controller/PortfolioController.php
+++ src/Investments/Application/Controller/PortfolioController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
+#[Route('/portfolio', name: 'app_full_portfolio')]
 class PortfolioController extends AbstractController
 {
     public function __construct(
@@ -23,7 +24,6 @@ class PortfolioController extends AbstractController
     ) {
     }
 
-    #[Route('/portfolio', name: 'app_full_portfolio')]
     public function __invoke(#[CurrentUser] ?User $user): Response
     {
         $compilerData = new PortfolioCompilerData(
