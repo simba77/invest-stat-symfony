557d4cb1c6171fba99ce001318fa7f966b6d5b0f
simba77 <simba7@inbox.ru>
2025-03-21 23:03:04 +0300

    feat: add logging of command execution time

---
+++ src/Shared/Infrastructure/Metrics/AppMetrics.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Metrics;
+
+class AppMetrics
+{
+    public const START_TIME = 'start_time';
+    public const END_TIME = 'end_time';
+    public const EXECUTION_TIME = 'execution_time';
+
+    private array $metrics = [];
+
+    public function __construct()
+    {
+        $this->metrics[self::START_TIME] = microtime(true);
+    }
+
+    public function getMetrics(): array
+    {
+        $this->metrics[self::END_TIME] = microtime(true);
+        $this->metrics[self::EXECUTION_TIME] = $this->metrics[self::END_TIME] - $this->metrics[self::START_TIME];
+
+        return $this->metrics;
+    }
+
+    public function getExecutionTime(): float
+    {
+        return round($this->getMetrics()[self::EXECUTION_TIME], 4);
+    }
+}
---
+++ src/Shared/Infrastructure/Symfony/CommandListener.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Symfony;
+
+use App\Shared\Infrastructure\Metrics\AppMetrics;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+#[AsEventListener(event: ConsoleEvents::TERMINATE)]
+class CommandListener
+{
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly AppMetrics $metrics,
+    ) {
+    }
+
+    public function __invoke(ConsoleTerminateEvent $event): void
+    {
+        $this->logger->info('The {command} command was completed in {time} sec.', [
+            'command' => $event->getCommand()?->getName() ?? 'unknown',
+            'time'    => $this->metrics->getExecutionTime(),
+        ]);
+    }
+}
