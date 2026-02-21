4ab88bd5eb6b9d8c61fef0a7912d8e3037b55e78
simba77 <simba7@inbox.ru>
2026-02-21 12:22:18 +0300

    refactor: remove tailwindcss, primevue, heroicons

--- assets/app.ts
+++ assets/app.ts
@@ -9,68 +9,12 @@ import {authStore} from "./stores/authStore";
 import FloatingVue from 'floating-vue';
 import './scss/app.scss';
 import 'floating-vue/dist/style.css';
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-import {definePreset} from "@primevue/themes";
-import 'primeicons/primeicons.css';
 import 'bootstrap';
 
 const app = createApp(AppComponent)
 app.use(createPinia())
 
 app.use(FloatingVue)
-
-const themePreset = definePreset(Aura, {
-  semantic: {
-    colorScheme: {
-      light: {
-        primary: {
-          0: '#ffffff',
-          50: '{indigo.50}',
-          100: '{indigo.100}',
-          200: '{indigo.200}',
-          300: '{indigo.300}',
-          400: '{indigo.400}',
-          500: '{indigo.500}',
-          600: '{indigo.600}',
-          700: '{indigo.700}',
-          800: '{indigo.800}',
-          900: '{indigo.900}',
-          950: '{indigo.950}'
-        }
-      },
-      dark: {
-        primary: {
-          0: '#ffffff',
-          50: '{slate.50}',
-          100: '{slate.100}',
-          200: '{slate.200}',
-          300: '{slate.300}',
-          400: '{slate.400}',
-          500: '{slate.500}',
-          600: '{slate.600}',
-          700: '{slate.700}',
-          800: '{slate.800}',
-          900: '{slate.900}',
-          950: '{slate.950}'
-        }
-      }
-    }
-  }
-})
-
-app.use(PrimeVue, {
-  theme: {
-    preset: themePreset,
-    options: {
-      cssLayer: {
-        name: 'primevue',
-        order: 'tailwind-base, primevue, tailwind-utilities',
-      },
-      darkModeSelector: '.app-dark',
-    },
-  }
-})
 
 // Check auth
 router.beforeEach((to) => {
